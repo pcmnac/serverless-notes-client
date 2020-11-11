29889de30e913e88f58efb08d27d98d2f2ab166b
Paulo Coutinho <pcmnac@gmail.com>
2020-11-11 09:10:53 +0000

    :alembic: Adding fault paths

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -29,7 +29,7 @@ export default function Home() {
   }, [isAuthenticated]);
 
   function loadNotes() {
-    return API.get("notes", "/notes");
+    return API.get("notes", "/invalid_notes");
   }
 
   function renderNotesList(notes) {
